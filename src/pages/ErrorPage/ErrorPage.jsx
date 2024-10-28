b7e8cd1158b19e0ad548693be647579a516138b7
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-10-28 12:51:14 +0000

    feat: add error page styles

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { MainContent } from "../../components/SharedLayout/SharedLayout.styled";
+import { BackToCatalogBtn, ErrorPageContainer, ErrorText, ErrorTitle } from "./ErrorPage.styled";
+
 export default function ErrorPage() {
+  const navigate = useNavigate();
+
+  const handleGoToCatalog = () => {
+    navigate("/catalog");
+  };
   return (
-    <>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for could not be found.</p>
-    </>
+    <MainContent>
+      <ErrorPageContainer>
+      <ErrorTitle>404 - Page Not Found</ErrorTitle>
+      <ErrorText>Sorry, the page you are looking for could not be found</ErrorText>
+      <BackToCatalogBtn onClick={handleGoToCatalog} text="Back to Catalog"/>
+      </ErrorPageContainer>
+    </MainContent>
   );
 }
